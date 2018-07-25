4dd0007de4bdde300b9623e8e41154fea4a0dfc0
AlexandraRadzkova <alexandra.radzkova@gmail.com>
2018-07-25 14:04:21 +0300

    selector for solar power link modified

--- pages/homePage.js
+++ pages/homePage.js
@@ -8,7 +8,11 @@ class HomePage extends Page {
             topNavigationContainer: element.all(By.className('top-navigation-container')),
             heroButtons: element.all(By.css('.flex-container-component__body .hero-button')),
             energy: element(By.css('.top-navigation-container>li:nth-child(3)')),
-            solarPowerLink: element(By.xpath("//a[contains(text(),'Solar Power')]")),
+            solarPowerLink: element(
+                By.xpath(
+                    "//div[contains(@class, 'header-links-section-component__section')]/a[text()='Solar Power']",
+                ),
+            ),
             moneyMadeEasyLink: element(
                 By.xpath(
                     "//div[contains(@class, 'links-section-component__section')]/a[text() = 'Money made easy']",
