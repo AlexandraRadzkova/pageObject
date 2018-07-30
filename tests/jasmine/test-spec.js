f44749757b167069baceb87ef11370f8de729cfc
AlexandraRadzkova <alexandra.radzkova@gmail.com>
2018-07-30 16:57:16 +0300

    jasmine test refactor

--- tests/jasmine/test-spec.js
+++ tests/jasmine/test-spec.js
@@ -1,111 +1,99 @@
-const HomePage = require('../../pages/HomePage')
-const CarInsurancePage = require('../../pages/carInsurancePage')
-const CarInsuranceFormPage = require('../../pages/carInsuranceFormPage')
-const SolarPowerPage = require('../../pages/solarPowerPage')
-const GasAndElectricityPage = require('../../pages/gasAndElectricityPage')
-const MoneyMadeEasyPage = require('../../pages/moneyMadeEasyPage')
-const ResoursesHubPage = require('../../pages/resoursesHubPage')
-const SavingsCalcPage = require('../../pages/savingsCalcPage')
-
-const homePage = new HomePage()
-const carInsurancePage = new CarInsurancePage()
-const carInsuranceFormPage = new CarInsuranceFormPage()
-const solarPowerPage = new SolarPowerPage()
-const gasAndElectricityPage = new GasAndElectricityPage()
-const moneyMadeEasyPage = new MoneyMadeEasyPage()
-const resoursesHubPage = new ResoursesHubPage()
-const savingsCalcPage = new SavingsCalcPage()
+const { pages, getCurrentPage, lastSymbol, removeLastSymbol } = require('../helpers/helpers')
 
 describe('test', () => {
     const EC = protractor.ExpectedConditions
     const isCurrentLicenceMonthVisible = EC.visibilityOf(
-        carInsuranceFormPage.data['currentLicenceMonth'],
+        pages.carInsuranceForm.data['currentLicenceMonth'],
     )
-    const isFeedbackButtonVisible = EC.visibilityOf(gasAndElectricityPage.data['feedbackButton'])
-    const isIframeVisible = EC.visibilityOf(gasAndElectricityPage.data['iframeRoot'])
+    const isFeedbackButtonVisible = EC.visibilityOf(pages.gasAndElectricity.data['feedbackButton'])
+    const isIframeVisible = EC.visibilityOf(pages.gasAndElectricity.data['iframeRoot'])
 
-    it('Fill car insurance form and click continue button', () => {
-        return homePage
+    fit('Fill car insurance form and click continue button', () => {
+        return pages.home
             .goToPage()
-            .then(() => homePage.clickElement('carInsuranceButton'))
+            .then(() => pages.home.clickElement('carInsuranceButton'))
             .then(() => {
                 return browser.getCurrentUrl().then(currentUrl => {
                     if (!currentUrl.includes('questionset')) {
                         console.log("doesn't containt questionset")
-                        return carInsurancePage.clickElement('getNewQuoteButton')
+                        return pages.carInsurance.clickElement('getNewQuoteButton')
                     }
                 })
             })
-            .then(() => carInsuranceFormPage.fillField('carRegistrationYear', '1753'))
+            .then(() => pages.carInsuranceForm.fillField('carRegistrationYear', '1753'))
             .then(() =>
-                carInsuranceFormPage.fillField(
+                pages.carInsuranceForm.fillField(
                     'address',
                     'Insure, 384, Clapham Road, London, SW99AR',
                 ),
             )
-            .then(() => carInsuranceFormPage.fillField('dateField', '29'))
-            .then(() => carInsuranceFormPage.fillField('monthField', '05'))
-            .then(() => carInsuranceFormPage.fillField('yearField', '1998'))
-            .then(() => carInsuranceFormPage.clickElement('kindOfDrivenLicense'))
-            .then(() => carInsuranceFormPage.clickElement('restrictionLast'))
-            .then(() => carInsuranceFormPage.selectDropdownValueByText('currentLicenceYear', '1'))
-            .then(() => carInsuranceFormPage.waitForEC(isCurrentLicenceMonthVisible, 5000))
-            .then(() => carInsuranceFormPage.selectDropdownValueByText('currentLicenceMonth', '5'))
-            .then(() => carInsuranceFormPage.clickElement('anyMedicalConditionsNo'))
-            .then(() => carInsuranceFormPage.clickElement('anyOtherCarsNo'))
-            .then(() => carInsuranceFormPage.clickElement('hasOffencesNo'))
-            .then(() => carInsuranceFormPage.clickElement('anyConvictionsNo'))
-            .then(() => carInsuranceFormPage.clickElement('hasInsuranceEverBeenDeclinedYes'))
+            .then(() => pages.carInsuranceForm.fillField('dateField', '29'))
+            .then(() => pages.carInsuranceForm.fillField('monthField', '05'))
+            .then(() => pages.carInsuranceForm.fillField('yearField', '1998'))
+            .then(() => pages.carInsuranceForm.clickElement('kindOfDrivenLicense'))
+            .then(() => pages.carInsuranceForm.clickElement('restrictionLast'))
+            .then(() => pages.carInsuranceForm.selectDropdownValueByText('currentLicenceYear', '1'))
+            .then(() => pages.carInsuranceForm.waitForEC(isCurrentLicenceMonthVisible, 5000))
             .then(() =>
-                carInsuranceFormPage.selectDropdownValueByText('yearsOfNoClaimsDiscount', '1'),
+                pages.carInsuranceForm.selectDropdownValueByText('currentLicenceMonth', '5'),
+            )
+            .then(() => pages.carInsuranceForm.clickElement('anyMedicalConditionsNo'))
+            .then(() => pages.carInsuranceForm.clickElement('anyOtherCarsNo'))
+            .then(() => pages.carInsuranceForm.clickElement('hasOffencesNo'))
+            .then(() => pages.carInsuranceForm.clickElement('anyConvictionsNo'))
+            .then(() => pages.carInsuranceForm.clickElement('hasInsuranceEverBeenDeclinedYes'))
+            .then(() =>
+                pages.carInsuranceForm.selectDropdownValueByText('yearsOfNoClaimsDiscount', '1'),
             )
             .then(() =>
-                carInsuranceFormPage.selectDropdownValueByPartialText(
+                pages.carInsuranceForm.selectDropdownValueByPartialText(
                     'startInsuranceDate',
                     'Today',
                 ),
             )
-            .then(() => carInsuranceFormPage.clickElement('continueButton'))
-            .then(() => expect(browser.getCurrentUrl()).toContain('aboutthecar'))
+            .then(() => pages.carInsuranceForm.clickElement('continueButton'))
+            .then(() => expect(pages.aboutTheCar.getUrl()).toContain('aboutthecar'))
     })
 
     it('Calculate savings', () => {
-        return homePage
+        return pages.home
             .goToPage()
-            .then(() => homePage.clickElement('moneyMadeEasyLink'))
-            .then(() => expect(moneyMadeEasyPage.getUrl()).toContain('money-made-easy'))
-            .then(() => moneyMadeEasyPage.clickElement('toolsAndCalc'))
-            .then(() => expect(resoursesHubPage.getUrl()).toContain('resources'))
-            .then(() => resoursesHubPage.clickElement('savingsCalc'))
-            .then(() => expect(savingsCalcPage.getUrl()).toContain('calculator'))
-            .then(() => savingsCalcPage.selectRadioButtonBySpace('saveEachMonthRadioButton'))
-            .then(() => savingsCalcPage.fillField('amountField', 500))
-            .then(() => savingsCalcPage.fillField('existingSavingsField', 300))
-            .then(() => savingsCalcPage.fillField('grossInterestRateField', 90))
-            .then(() => savingsCalcPage.clickElement('calcMySavingsButton'))
+            .then(() => pages.home.clickElement('moneyMadeEasyLink'))
+            .then(() => expect(pages.moneyMadeEasy.getUrl()).toContain('money-made-easy'))
+            .then(() => pages.moneyMadeEasy.clickElement('toolsAndCalc'))
+            .then(() => expect(pages.resoursesHub.getUrl()).toContain('resources'))
+            .then(() => pages.resoursesHub.clickElement('savingsCalc'))
+            .then(() => expect(pages.savingsCalc.getUrl()).toContain('calculator'))
+            .then(() => pages.savingsCalc.selectRadioButtonBySpace('saveEachMonthRadioButton'))
+            .then(() => pages.savingsCalc.fillField('amountField', 500))
+            .then(() => pages.savingsCalc.fillField('existingSavingsField', 300))
+            .then(() => pages.savingsCalc.fillField('grossInterestRateField', 90))
+            .then(() => pages.savingsCalc.clickElement('calcMySavingsButton'))
             .then(() =>
-                expect(savingsCalcPage.isElementVisible('saveEachMonthResults')).toBeTruthy(),
+                expect(pages.savingsCalc.isElementVisible('saveEachMonthResults')).toBeTruthy(),
             )
     })
 
     it('Leave feedback on Gas and Electricity Page', () => {
-        return homePage
+        return pages.home
             .goToPage()
-            .then(() => homePage.mouseMoveToElement('energy'))
-            .then(() => homePage.mouseMoveToElement('solarPowerLink'))
-            .then(() => homePage.clickElement('solarPowerLink'))
-            .then(() => solarPowerPage.clickElement('switchAndSaveNowButton'))
-            .then(() => gasAndElectricityPage.waitForEC(isFeedbackButtonVisible, 5000))
-            .then(() => gasAndElectricityPage.clickElement('feedbackButton'))
-            .then(() => gasAndElectricityPage.waitForEC(isIframeVisible, 5000))
-            .then(() => gasAndElectricityPage.switchToIframe())
-            .then(() => gasAndElectricityPage.clickElement('generalFeedback'))
-            .then(() => gasAndElectricityPage.switchToDefaultContent())
-            .then(() => gasAndElectricityPage.switchToIframe())
-            .then(() => gasAndElectricityPage.clickElement('likeRadioButton'))
-            .then(() => gasAndElectricityPage.fillField('feedbackTextField', 'LIKE'))
-            .then(() => gasAndElectricityPage.clickElement('submitButton'))
-            .then(() => expect(gasAndElectricityPage.isElementVisible('successForm')).toBeTruthy())
-            .then(() => gasAndElectricityPage.switchToDefaultContent())
+            .then(() => pages.home.mouseMoveToElement('energy'))
+            .then(() => pages.home.mouseMoveToElement('solarPowerLink'))
+            .then(() => pages.home.clickElement('solarPowerLink'))
+            .then(() => pages.solarPower.clickElement('switchAndSaveNowButton'))
+            .then(() => pages.gasAndElectricity.waitForEC(isFeedbackButtonVisible, 5000))
+            .then(() => pages.gasAndElectricity.clickElement('feedbackButton'))
+            .then(() => pages.gasAndElectricity.waitForEC(isIframeVisible, 5000))
+            .then(() => pages.gasAndElectricity.switchToIframe())
+            .then(() => pages.gasAndElectricity.clickElement('generalFeedback'))
+            .then(() => pages.gasAndElectricity.switchToDefaultContent())
+            .then(() => pages.gasAndElectricity.switchToIframe())
+            .then(() => pages.gasAndElectricity.clickElement('likeRadioButton'))
+            .then(() => pages.gasAndElectricity.fillField('feedbackTextField', 'LIKE'))
+            .then(() => pages.gasAndElectricity.clickElement('submitButton'))
+            .then(() =>
+                expect(pages.gasAndElectricity.isElementVisible('successForm')).toBeTruthy(),
+            )
+            .then(() => pages.gasAndElectricity.switchToDefaultContent())
     })
 })
