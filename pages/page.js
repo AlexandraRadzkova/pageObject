868317202197e28be45100ac42b8c4303f626f19
AlexandraRadzkova <alexandra.radzkova@gmail.com>
2018-07-31 11:32:21 +0300

    refactor

--- pages/page.js
+++ pages/page.js
@@ -1,11 +1,14 @@
 class Page {
     constructor() {}
+
     getUrl() {
         return browser.getCurrentUrl()
     }
+
     goToPage() {
         return browser.get(this.url)
     }
+
     isElementVisible(name) {
         return this.data[name].isDisplayed()
     }
